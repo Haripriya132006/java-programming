58be96d34af38dec3a8459644e09aa45234a08c7
Haripriya132006 <haripriyaks13@gmail.com>
2025-04-11 13:22:59 +0530

    Success

--- duplicate.java
+++ duplicate.java
@@ -1,3 +1,24 @@
+import java.util.*;
 public class duplicate {
-    public static void(main)
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter number of elements");
+        int noOfElements=input.nextInt();
+        int[] list= new int[noOfElements];
+        for(int i=0;i<noOfElements;i++){
+            System.out.print("enter element "+(i+1)+ " ");
+            list[i]=input.nextInt();
+        }
+
+        for(int i=0;i<noOfElements;i++){
+            for(int j=0;j<noOfElements;j++){
+                if(i!=j && list[i]==list[j]){
+                    System.out.println("contains duplicates");
+                    return;
+                }
+            }
+        }
+        System.out.println("no duplicates found");
+        
+    }
 }
